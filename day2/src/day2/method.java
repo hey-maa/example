8618cdcfe8f388edfd646c401805551ed733eb82
hey-maa <112340310+hey-maa@users.noreply.github.com>
2024-06-27 10:24:59 +0530

    PRE AND POST

--- day2/src/day2/method.java
+++ day2/src/day2/method.java
@@ -2,7 +2,14 @@ package day2;
 
 public class method {
 	public static void main(String [] args) {
-		System.out.print("hello world");
+		System.out.println("hello world");
+		int a =100;
+		int c = a++;
+		
+		System.out.println(a);
+		System.out.println(c);
+	
+		
 	}
 
 }
